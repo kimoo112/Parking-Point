7aad9c586b4eb75fb9962e0fe031ac247006b05e
kimoo112 <kimo8123008@gmail.com>
2024-04-27 01:40:49 +0200

    Unlock Api Keys and remove it from gitignore

---
+++ lib/core/utils/api_keys.dart
@@ -0,0 +1,6 @@
+abstract class APiKeys {
+  static const paypalClientId =
+      'AQhIn3tcEOWVLJ1wNv6oOFxhXc8_BC8-Wvrd_Lb_XL2b2_IydrG4j9HoATTVneOaAYlgn4R5GHWCiIuZ';
+  static const paypalSecretKey =
+      'EDJAm58rNHsORxu6cD25LdTp4XurdgS--iT7feKTQ5x1fQkNAA5RTgB-8wPfjDXoKE8Zc5U8d-fqZGZF';
+}
